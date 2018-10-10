e8d9e4f5d62f422f1d68c91a889385312b45f698
Soheima Canton <soheima@Soheimas-MacBook-Pro.local>
2018-10-10 10:47:28 +0200

    app setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const express = require('express')
+const bcrypt = require('bcrypt');
+const session = require('express-session')
+
+const app = express()
+
