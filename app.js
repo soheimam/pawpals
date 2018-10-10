148b75d7f90208c8069196932afbe1b3ae789ddc
Berenice <berebarcenarm@gmail.com>
2018-10-10 16:33:01 +0200

    updated login

--- app.js
+++ app.js
@@ -125,6 +125,51 @@ app.post('/signup', (req, res) => {
     });
 });
 
+//post request login
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //email and password is needed
+  if (!email) {
+    res.redirect(
+      '/login?message=' + encodeURIComponent('Please fill out your email address.')
+    );
+  }
+  if (!password) {
+    res.redirect(
+      '/login?message=' + encodeURIComponent('Please fill out your password.')
+    );
+  }
+  //find the user with the email from the req
+  User.findOne({
+    where: {
+      email: email,
+    },
+  })
+    .then(user => {
+      if (user) {
+        bcrypt.compare(password, user.password).then(isValidPassword => {
+          if (isValidPassword) {
+            req.session.user = user;
+            res.redirect('/profile');
+          } else {
+            //if the password is incorrect, send a message
+            res.redirect(
+              '/login?message=' + encodeURIComponent('Invalid  password.')
+            );
+          }
+        });
+      } else {
+        //if user does not exist, send a message
+        res.redirect('/login?message=' + encodeURIComponent('User does not exist'));
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+
 //logout route
 app.get('/logout', (req, res) => {
   req.session.destroy(error => {
