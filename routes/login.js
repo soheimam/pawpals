007b38f24c18ecc74fe7c98a74b38686c8f1c3d2
Berenice <berebarcenarm@gmail.com>
2018-10-22 21:06:23 +0200

    Replace bcrypt with bcryptjs

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 
 const get = (req, res) => {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { User, Dog, Match, Conversation } = require('../models');
 
 //create a new user from the signup form
