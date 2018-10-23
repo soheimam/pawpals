87d9d1650d7456da25029b9d1df6a47bf65137c5
Soheima Canton <soheima@Soheimas-MacBook-Pro.local>
2018-10-23 15:08:22 +0200

    gallery view

---
+++ views/javascript/nav.js
@@ -0,0 +1,16 @@
+// Get the container element
+var nav = document.getElementById("navbar");
+
+var btns = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    this.className += " active";
+  });
+}
